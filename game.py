f367847b4c578cf78ca8b74e3734f1da7f8522e7
Addy768 <adityasharma02092002@gmail.com>
2022-10-10 13:02:04 -0400

    uploadv2

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
-'''Hi player welcome to the to WIzard fighting game this game is inspired by Legendary movie series Harry potter.
-This game will let you experience the chronicles of harry potter through a adventarous text based game.
-SO SHALL WE BEGIN?? OR YOU ARE SCARED'''
+print('''Hi player welcome to the to WIzard fighting game this game is inspired by Legendary movie series Harry potter.
+This game will let you experience the chronicles of harry potter through a very adventarous text based game.
+SO SHALL WE BEGIN?? OR YOU ARE SCARED''' )
 
 from ast import If
 from cgi import print_arguments
@@ -11,72 +11,55 @@ import time
 
 ##dice
 import random
+from tkinter import N
+
 from unicodedata import name
- 
- 
-x = "y"
-  
-while x == "y":
-     
 
-    #CHARACTERS ACCORDING TO YOU RESULT
-    '''
-    
-       1= Harry Potter
-       2= Rupert Grint
-       3= Hermione Granger
-       4= Draco Malfoy
-       5= Rubeus Hagrid
-       6= Albus Dumbledore'''
+import dice
+gm=dice.dicing()
+gm.dice()
+
+### character information
+a= input("press 1 for infpormation")
+import albus
+import hermoine
+import rupert
+import dracomalfoy
+import rubeus
+import harry
 
 
+if(a=="1"):
+    albuss=albus.name1()
+    albuss.albusprint()
+
+b= input("press 2 for information")
+if(b=="2"):
+    dracoo=dracomalfoy.name2()
+    dracoo.dracoprint()
+
+c= input("press 3 for information")
+if(c=="3"):
+    hary=harry.name3()
+    hary.harryprint()
+
+d= input("press 4 for information")
+if(d=="4"):
+    hermoinee=hermoine.name4()
+    hermoinee.hermoineprint()
+
+e= input("press 5 for information")
+if(e=="5"):
+    rubeuss=rubeus.name5()
+    rubeuss.rubeusprint()
+
+f= input("press 6 for infpormation")
+if(f=="6"):
+    rupertt=rupert.name6()
+    rupertt.rupertprint()
 
 
-    no = random.randint(1,6)
-     
-    if no == 1:
-        print("[-----]")
-        print("[     ]")
-        print("[  0  ]")
-        print("[     ]")
-        print("[-----]")
-    if no == 2:
-        print("[-----]")
-        print("[ 0   ]")
-        print("[     ]")
-        print("[   0 ]")
-        print("[-----]")
-    if no == 3:
-        print("[-----]")
-        print("[     ]")
-        print("[0 0 0]")
-        print("[     ]")
-        print("[-----]")
-    if no == 4:
-        print("[-----]")
-        print("[0   0]")
-        print("[     ]")
-        print("[0   0]")
-        print("[-----]")
-    if no == 5:
-        print("[-----]")
-        print("[0   0]")
-        print("[  0  ]")
-        print("[0   0]")
-        print("[-----]")
-    if no == 6:
-        print("[-----]")
-        print("[0 0 0]")
-        print("[     ]")
-        print("[0 0 0]")
-        print("[-----]")
-         
-    x=input("press y to roll again and n to exit:")
-    print("\n")
-
-
-
-#animating the welcome text for game
+#text will be used to welcome to game,exit the game,when it draws or the specialty the player
 
 wizard="""
  __          __  _                           __          ___                  _ 
@@ -84,7 +67,7 @@ wizard="""
   \ \  /\  / /__| | ___ ___  _ __ ___   ___   \ \  /\  / / _ ______ _ _ __ __| |
    \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \   \ \/  \/ / | |_  / _` | '__/ _` |
     \  /\  /  __/ | (_| (_) | | | | | |  __/    \  /\  /  | |/ / (_| | | | (_| |
-     \/  \/ \___|_|\___\___/|_| |_| |_|\___|     \/  \/   |_/___\__,_|_|  \__,_
+     \/  \/ \___|_|\___\___/|_| |_| |_|\___|     \/  \/   |_/___\__,_|_|  \__,_|
 """
 witch="""
  __          __  _                           __          ___ _       _     
@@ -201,12 +184,101 @@ def game(name):
         print(f"\n\n{name} healed themselves with {user_spell}")
         print("Health Healed")
         user_health+=health
-        #print(user_health)
+        ##print the(user_health)
     if user_spell>="e"and user_spell<="h" and comp_spell in attack_spells:
-        print(f"\n\nEnemy attacked with {comp_spell} which {name} defended with {user_spell}")
+        print(f"\n\nhahaha Enemy attacked with {comp_spell} which {name} defended with {user_spell}")
         print(f"0 Damage done to you")
         print("0 Damage Done to you")
     if comp_spell in attack_spells:
         print(f"\n\nEnemy attacked with {comp_spell}")
         print("Damage dealt to You")
         user_health-=damage
+          ###print the(user_health)
+    elif comp_spell in healing_spells:
+        print(f"\n\nEnemy healed themselves with {comp_spell}")
+        print("Damage healed by enemy")
+        comp_health+=health
+        ##print the(comp_health)
+    if comp_spell in defense_spells and user_spell>="0" and user_spell<="9":
+        print(f"\n\n{name} attacked with {attack_spells[int(user_spell)-1]} which the enemy defended with {comp_spell}")
+        print(f"\n\n0 Damage done to enemy")
+        print("0 damage done to enemy")
+    if comp_spell=="Imperio":
+        user_health-=30
+        print(f"\n\nEnemy attacked you with the {comp_spell} Unforgivable Curse.")
+        print("High Damage dealt to you")
+    elif comp_spell=="Crucio":
+        user_health-=50
+        print(f"\n\nEnemy attacked you with the {comp_spell} Unforgivable Curse.")
+        print("High Damage dealt to you")
+    elif comp_spell=="Avada-Kedavara":
+        user_health-=80
+        print(f"\n\nEnemy attacked you with the {comp_spell} Unforgivable Curse.")
+        print("High Damage dealt to you")
+         ##print(comp_spell)
+
+    win(name)
+def win(name):
+    global comp_health
+    global user_health
+    if comp_health<=0 and user_health<=0:
+        print(draw)
+        print("Battle Draw")
+        comp_health=0
+        user_health=0
+        exit_game()
+    elif comp_health<=0 and user_health!=0:
+        print(user_win)
+        print("You Win!!")
+        comp_health=0
+        exit_game()
+    elif comp_health!=0 and user_health<=0:
+        print(enemy_win)
+        print("Enemy won!!!")
+        user_health=0
+        exit_game()
+
+        
+#####importing error in app file
+
+print(logo)
+name=input("Enter Your Name according to the number you got from dice roll: ")
+choice=input("\n\nAre you a witch or a wizard? ").lower()
+difficulty=input("Please choose a difficulty. Type 'easy' for easy  but beware you wont face any real challange now thats on you to decide difficulty but if you are brave and courageous press 'hard' for hard difficulty: ").lower()
+if difficulty=="easy":
+        print("You have chosen easy difficulty, Enemy won't be able to use Unforgivable Curses.")
+else:
+        print("You have chosen hard difficulty, Enemy will be able to use Unforgivable Curses.")
+
+if choice=="wizard":
+        print(wizard)
+    
+        print("hahahaha Welcome Wizard. Let's Fight....") 
+    
+elif choice=="witch":
+        print(witch)
+        print(wizard)
+        print("hahahaha Welcome Witch. Let's Fight.....")
+    
+else:
+        print("Wrong Choice!!!")    
+print("Below are the instructions to play the game:")
+print("\n\nEnter any key from 1 to 10 for Attack spells ")
+print(attack_spells)
+print("\n\nPress any key from A,B,C,D to heal yourself.")
+print(healing_spells)
+print("\n\nPress any key from E,F,G,H to protect/shield yourself. ")
+print(defense_spells)
+print("\n\nEach key contains a different spell. So don't be afraid to press different keys. ")
+ch=input("\n\nReady to Play? Press 'y' to Play the game.")
+os.system('clear')
+   
+
+    
+print(logo)
+while should_continue:
+    
+    game(name)
+print(f"Final Health of {name}: {user_health}")
+print(f"Final Health of Enemy: {comp_health}")
+
---
+++ harry.py
@@ -0,0 +1,13 @@
+class name3:
+    def harryprint(self):
+        
+        print('''Harry James Potter (born July 31, 1980 ) is an English wizard and one of the most famous wizards of the contemporary age.
+         He became famous from an early age for having survived the Killing Curse , hurled by Lord Voldemort in an attempt to kill him to avoid the fulfillment of a prophecy.
+         This event caused Voldemort's first defeat and the end of the First Wizarding War , resulting in Harry being transferred to Muggle maternal relatives , the Dursleys , in order to protect him. From that moment Harry became known throughout the wizarding world as "the child who survived".''')
+
+
+
+
+
+
+
